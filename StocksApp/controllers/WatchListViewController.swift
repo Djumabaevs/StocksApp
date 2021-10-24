4cb57f53d97c390cf34b561cb7a9861165a8272e
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-25 00:56:22 +0600

    working properly and switching light to dark mode

--- StocksApp/controllers/WatchListViewController.swift
+++ StocksApp/controllers/WatchListViewController.swift
@@ -12,6 +12,7 @@ class WatchListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
      
+        view.backgroundColor = .tertiarySystemBackground
     }
 
 
