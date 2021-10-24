139e78e55a53d26abcaedbb97232a3a09fcc2423
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-25 00:43:29 +0600

    top news stories controller

---
+++ StocksApp/controllers/TopStoriesNewsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TopStoriesNewsViewController.swift
+//  StocksApp
+//
+//  Created by Bakyt Dzhumabaev on 25/10/21.
+//
+
+import UIKit
+
+class TopStoriesNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+ 
+
+}
