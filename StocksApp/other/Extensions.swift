271a87a93078b40ab7dc21f19350e027ed65a685
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-26 00:04:14 +0600

    framing extension

--- StocksApp/other/Extensions.swift
+++ StocksApp/other/Extensions.swift
@@ -12,6 +12,27 @@ import UIKit
 
 extension UIView {
     
-    var 
+    var width: CGFloat {
+        frame.size.width
+    }
     
+    var height: CGFloat {
+        frame.size.height
+    }
+    
+    var left: CGFloat {
+        frame.origin.x
+    }
+    
+    var right: CGFloat {
+        left + width
+    }
+    
+    var top: CGFloat {
+        frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        top + height
+    }
 }
