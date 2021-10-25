9094c264b8741d880886d39d069dace44fdea872
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-26 00:22:03 +0600

    required init

---
+++ StocksApp/views/SearchResultTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  SearchResultTableViewCell.swift
+//  StocksApp
+//
+//  Created by Bakyt Dzhumabaev on 26/10/21.
+//
+
+import UIKit
+
+class SearchResultTableViewCell: UITableViewCell {
+    static let identifier = "SearchResultTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
