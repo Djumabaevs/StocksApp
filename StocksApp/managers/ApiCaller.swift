378cf7978be4b1653f270d889082a330d6ececf8
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-25 19:50:08 +0600

    generic function to handle api requests

--- StocksApp/managers/ApiCaller.swift
+++ StocksApp/managers/ApiCaller.swift
@@ -10,6 +10,8 @@ import Foundation
 final class ApiCaller {
     static let shared = ApiCaller()
     
+    private
+    
     private init() {}
     
     //MARK: - Public
@@ -24,6 +26,11 @@ final class ApiCaller {
         case search
     }
     
+    private enum ApiError: Error {
+        case noDataReturned
+        case invalidUrl
+    }
+    
     private func url(
         for endpoint: Endpoint,
         queryParams: [String: String] = [:]
@@ -32,5 +39,35 @@ final class ApiCaller {
         return nil
     }
     
-    
+    private func request<T: Codable>(
+        url: URL?,
+        expecting: T.Type,
+        completion: @escaping(Result<T, Error>) -> Void) {
+        
+        guard let url = url else {
+            //Invalid url
+            completion(.failure(ApiError.invalidUrl))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(ApiError.noDataReturned))
+                }
+                return
+            }
+            
+            do {
+                let  result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
 }
