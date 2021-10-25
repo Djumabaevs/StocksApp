a5e5e45b79cb88380e4db791b8612b5d97c07d1e
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-26 00:15:23 +0600

    view did layout subviews

--- StocksApp/controllers/SearchResultsViewController.swift
+++ StocksApp/controllers/SearchResultsViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class SearchResultsViewController: UIViewController {
+    
+    private let tableView: UITableView {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
 
     }
     
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        
+    }
 
 }
