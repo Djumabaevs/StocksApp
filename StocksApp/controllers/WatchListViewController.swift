73bae123aa2213b78b1aa0f935db0c0cf35ec7bb
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-25 21:24:20 +0600

    search query validation

--- StocksApp/controllers/WatchListViewController.swift
+++ StocksApp/controllers/WatchListViewController.swift
@@ -13,8 +13,25 @@ class WatchListViewController: UIViewController {
         super.viewDidLoad()
      
         view.backgroundColor = .tertiarySystemBackground
+        setupSearchController()
     }
 
+    private func setupSearchController() {
+        let resultVC = SearchResultsViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+}
 
+extension WatchListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        print(query)
+    }
 }
 
