d98120c0f3a562a5902f470401489714cdb799db
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-27 00:40:40 +0600

    struct search response as model

---
+++ StocksApp/models/SearchResponse.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResponse.swift
+//  StocksApp
+//
+//  Created by Bakyt Dzhumabaev on 27/10/21.
+//
+
+import Foundation
+
+struct SearchResponse: Codable {
+    
+}
--- StocksApp/other/AppDelegate.swift
+++ StocksApp/other/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-        ApiCaller.shared.search(query: "Pets from AppDelegate") { _ in
+        ApiCaller.shared.search(query: "Apple") { _ in
             
         }
         
