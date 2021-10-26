6de6ed74502fe5581ee11d125058467a4a29491c
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-27 00:45:55 +0600

    struct search result from json

--- StocksApp/models/SearchResponse.swift
+++ StocksApp/models/SearchResponse.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 struct SearchResponse: Codable {
-    
+    let count: Int
+    let result: [SearchResult]
+}
+
+struct SearchResult: Codable {
+    let description: String
+    let displaySymbol: String
+    let symbol: String
+    let type: String
 }
