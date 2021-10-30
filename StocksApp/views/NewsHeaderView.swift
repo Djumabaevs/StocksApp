f951926386bf7c4d3b0999fbf8169ef0afe47b4a
BakytDzhumabaev <djumabaevb@gmail.com>
2021-10-30 12:00:38 +0600

    view model

--- StocksApp/views/NewsHeaderView.swift
+++ StocksApp/views/NewsHeaderView.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class NewsHeaderView: UITableViewHeaderFooterView {
+    static let identifier = "NewsHeaderView"
+    static let preferredHeight: CGFloat = 70
     
+    struct ViewModel {
+        let title: String
+        let shouldShowAddButton
+    }
 
 }
